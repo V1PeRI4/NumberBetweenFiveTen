16e66b299c89737ba803032f67ef06fc386eb07f
V1PeRI4 <komardenkovt@gmail.com>
2023-01-12 14:16:15 +0300

    Добавьте файлы проекта.

---
+++ NumberBetweenFiveTen/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberBetweenFiveTen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string numString = Console.ReadLine();
+
+            try
+            {
+                float num = float.Parse(numString);
+
+                if (num > 5 && num < 10)
+                    Console.WriteLine($"Число больше 5 и меньше 10");
+                else
+                    Console.WriteLine($"Неизвестное число");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
